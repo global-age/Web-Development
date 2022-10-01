53b88abee7f75fd13cdcb5f635e243d5ae5b5ba0
global-age <globalage595@gmail.com>
2022-10-01 09:03:36 -0700

    add javascvript lecture no 2

---
+++ Js-Lectureno-1/firstJavascriptFile.js
@@ -0,0 +1,38 @@
+// primitive
+
+// 1 Number
+// 2 string
+// 3 undefined
+// 4 Boolean
+// 5 nulll
+
+// derived
+
+// let Human = {
+//     name:String,
+//     age:Number,
+//     height:Number,
+//     Male:Boolean,
+//     Female:Boolean,
+
+//     CanEat(){
+//         return Boolean
+//     }
+
+//     CanTeach(){
+//         retrun Boolean
+//     }
+
+// }
+
+// let var consta
+
+
+// +,-,*,/,% (artimetic operator)
+// increment and decrement operatrors x++, x--
+// assignemt = ;
+// logical operators  &&, || , !
+
+// COMPARISON OPERATOR > < >=  <= == ===
+// let x = 10 ;
+
